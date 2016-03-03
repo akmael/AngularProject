f3c26770c30b1351cda9a2d64ebcb949e729fd00
AndraIshmael <ishmaelak@gmail.com>
2016-03-03 18:19:19 -0500

    forgot to commit

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,26 @@ var app = angular.module('ngTracker', []);
 
 
 app.controller('TrackerController', function($scope){
-  $scope.test ="Hello world";
+   //place holder array 1 of 3
+   $scope.jobList = ['Target',
+                    'Walmart',
+                    'capsule corp',
+                    'umbrella corp',
+                    'Valve'];
 
+   /// adds to job list of yet to apply
+   $scope.saveData = function(){
+     var jobName = $scope.currentData;
+     $scope.jobList.push(jobName);
+     $scope.currentData="";
+   }
+
+  //remove from list
+  $scope.deleteData = function($index){
+
+    $scope.jobList.splice($index, 1)
+
+  }
 
 
 });
